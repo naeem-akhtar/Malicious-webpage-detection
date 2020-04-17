fa9fedd3731a87407463fd7bb737dce16bf0a668
naeem-akhtar <nakhtar.7777@gmail.com>
2020-04-17 19:51:06 +0530

    added the functionality to classify urls

--- try.py
+++ try.py
@@ -1,29 +1,9 @@
-import Feature_Extraction
-import numpy as np
-import pandas as pd
+from prediction import predict
 
-Invalid_urls = []
 
-df = pd.read_csv('final_dataset.csv')
-
-print(df.columns)
-i , j = 0, 0
-for url in df['url']:
-    input('press Any key to extract feature from next url')
+for _ in range(int(input('Enter number of urls: '))):
     try:
-        # print(i, df['target'][i], url)   # Might give error because of unidentified characters in url
-        if df['target'][i+j] == 0 and i<50:
-            i += 1
-            print(Feature_Extraction.vector_construction(url), end='\n\n')
-        elif df['target'][i+j] == 1 and j<50:
-            j += 1
-            print(Feature_Extraction.vector_construction(url), end='\n\n')
-        else:
-            break
+        print('Malicious :(' if predict(input()) else 'Safe :)')
     except:
-        Invalid_urls.append(i)
-        print("Cannot read this url")
-    
-print(df.columns)
-
-print(Invalid_urls)
+        print("Cannot check this url :(")
+        break
